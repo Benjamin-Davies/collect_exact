11a033eab789d5c8f63f64949dd791f8299cee70
Benjamin Davies <bentendavies@gmail.com>
2025-03-22 12:29:54 +1300

    feat: traits for Error

--- src/errors.rs
+++ src/errors.rs
@@ -1,4 +1,18 @@
+use std::{error, fmt};
+
+#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub enum Error {
     TooFewItems,
     TooManyItems,
+}
+
+impl error::Error for Error {}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Error::TooFewItems => write!(f, "too few items"),
+            Error::TooManyItems => write!(f, "too many items"),
+        }
+    }
 }
