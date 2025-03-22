8d43dd7809d43a81b210522206d20c8e9731aa81
Benjamin Davies <bentendavies@gmail.com>
2025-03-22 13:44:23 +1300

    docs: add SAFETY comments

--- src/impls.rs
+++ src/impls.rs
@@ -15,21 +15,26 @@ pub(crate) fn collect_exact<T, const N: usize>(
 pub(crate) fn collect_exact_prefix<T, const N: usize>(
     mut iter: impl Iterator<Item = T>,
 ) -> Result<[T; N], crate::PrefixError> {
-    unsafe {
-        let mut array = MaybeUninit::<[T; N]>::uninit();
+    let mut array = MaybeUninit::<[T; N]>::uninit();
 
-        let start_ptr = array.as_mut_ptr().cast::<T>();
-        for i in 0..N {
-            if let Some(item) = iter.next() {
-                start_ptr.add(i).write(item);
-            } else {
-                for j in 0..i {
-                    start_ptr.add(j).drop_in_place();
-                }
-                return Err(crate::PrefixError);
+    let start_ptr = array.as_mut_ptr().cast::<T>();
+    for i in 0..N {
+        if let Some(item) = iter.next() {
+            // SAFETY: We have ensured that `i` is less than `N`.
+            unsafe { start_ptr.add(i).write(item) };
+        } else {
+            // Drop any items we have initialized.
+            for j in 0..i {
+                // SAFETY: All elements up to `i` have been initialized.
+                unsafe { start_ptr.add(j).drop_in_place() };
             }
-        }
 
-        Ok(array.assume_init())
+            return Err(crate::PrefixError);
+        }
     }
+
+    // SAFETY: If we reach this point, then all elements have been initialized.
+    let array = unsafe { array.assume_init() };
+
+    Ok(array)
 }
